6ba4b18f61d092087179acbbfbafe2c2d40fd041
Diogo Vilela <90631238+DiogoVilela12@users.noreply.github.com>
2023-06-14 16:49:01 -0300

    Iniciando 768px layout

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ function mostrarMenu(elemento) {
     elemento = document.getElementById(elemento);
 
     if (elemento.style.visibility == 'visible') {
-        document.getElementById("header").style.height = "2.5rem";
         elemento.style.visibility = 'hidden'
+        document.getElementById("cabecalho").style.height = "2.5rem";
 
     } else {
-        document.getElementById("header").style.height = "5rem";
         elemento.style.visibility = 'visible'
-        document.getElementById("header").style.height = "12.5rem";
+        document.getElementById("cabecalho").style.height = "12.5rem";
 
     }
-    document.getElementById('botao__menu').style.display = 'flex'
 }
 
 function esconderMenu(elemento) {
-    document.getElementById("header").style.height = "2.5rem";
-    elemento = document.getElementById(elemento);
-    elemento.style.visibility = 'hidden'
+    var elemento = document.getElementById(elemento);
+
+    if (window.matchMedia("(min-width: 768px)").matches == false) {
+        document.getElementById("cabecalho").style.height = "2.5rem";
+        elemento.style.visibility = 'hidden'
+    }
 }
